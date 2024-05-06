73d83b44d85b5af2c4c815d823a2139ff871c1f8
itsjaylen <bossjaylen145@gmail.com>
2024-05-06 16:02:24 -0400

    fix hopefully

--- services/backend/migrations/2024-04-24-233819_create_widget_position/up.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE widgets (
-    widget_id SERIAL PRIMARY KEY,
-    widget_name VARCHAR(100) NOT NULL,
-    position_x INTEGER,
-    position_y INTEGER
-);
--- services/backend/migrations/2024-04-25-001732_update_widgets/up.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE widgets
-ADD COLUMN embed BOOLEAN,
-ADD COLUMN interactive BOOLEAN,
-ADD COLUMN opacify INTEGER CHECK (opacify >= 0 AND opacify <= 100),
-ADD COLUMN position_z INTEGER CHECK (position_z >= 0 AND position_z <= 180),
-ADD COLUMN blur INTEGER CHECK (blur >= 0 AND blur <= 100);
